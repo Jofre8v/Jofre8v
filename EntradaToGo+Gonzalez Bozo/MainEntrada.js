c45f45acbed5bc80d900ec6da95c2fe5d5a2e22c
Jofre8v <156092741+Jofre8v@users.noreply.github.com>
2024-10-20 18:23:10 -0300

    Add files via upload

--- EntradaToGo+Gonzalez Bozo/MainEntrada.js
+++ EntradaToGo+Gonzalez Bozo/MainEntrada.js
@@ -1,79 +1,78 @@
-const database =  [
-    { nombre: "Slipknot", precio: 500 },
-    { nombre: "Metallica", precio: 600 },
-    { nombre: "Nirvana", precio: 550 },
-    { nombre: "Slipknot", precio: 500 },
-    { nombre: "Metallica", precio: 600 },
-    { nombre: "Nirvana", precio: 550 },
-    { nombre: "AC/DC", precio: 700 },
-    { nombre: "Led Zeppelin", precio: 650 },
-    { nombre: "Pink Floyd", precio: 680 },
-    { nombre: "Queen", precio: 750 },
-    { nombre: "The Beatles", precio: 800 },
-    { nombre: "The Rolling Stones", precio: 720 },
-    { nombre: "Guns N' Roses", precio: 670 },
-    { nombre: "Linkin Park", precio: 600 },
-    { nombre: "Green Day", precio: 590 },
-    { nombre: "The Who", precio: 710 },
-    { nombre: "Aerosmith", precio: 640 },
-    { nombre: "Red Hot Chili Peppers", precio: 630 },
-    { nombre: "Foo Fighters", precio: 620 },
-    { nombre: "Rammstein", precio: 680 },
-    { nombre: "The Doors", precio: 690 },
-    { nombre: "KISS", precio: 610 },
-    { nombre: "Pearl Jam", precio: 650 },
-    { nombre: "The Smashing Pumpkins", precio: 620 },
-    { nombre: "Deep Purple", precio: 670 },
-    { nombre: "Black Sabbath", precio: 750 },
-    { nombre: "Iron Maiden", precio: 700 },
-    { nombre: "System of a Down", precio: 580 },
-    { nombre: "Soundgarden", precio: 590 },
-    { nombre: "Pantera", precio: 640 },
-    { nombre: "Megadeth", precio: 680 },
-    { nombre: "Judas Priest", precio: 710 },
-    { nombre: "Rage Against the Machine", precio: 650 },
-    { nombre: "Avenged Sevenfold", precio: 630 },
-    { nombre: "Muse", precio: 620 },
-    { nombre: "Radiohead", precio: 690 },
-    { nombre: "The Strokes", precio: 600 },
-    { nombre: "The Killers", precio: 620 },
-    { nombre: "Arctic Monkeys", precio: 610 },
-    { nombre: "Motörhead", precio: 700 },
-    { nombre: "Disturbed", precio: 650 },
-    { nombre: "Evanescence", precio: 620 },
-    { nombre: "Marilyn Manson", precio: 630 },
-    { nombre: "The Cure", precio: 680 },
-    { nombre: "Fall Out Boy", precio: 610 },
-    { nombre: "Bring Me the Horizon", precio: 590 },
-    { nombre: "Blink-182", precio: 600 },
-    { nombre: "Paramore", precio: 580 },
-    { nombre: "Thirty Seconds to Mars", precio: 620 },
-    { nombre: "Breaking Benjamin", precio: 590 },
-    { nombre: "My Chemical Romance", precio: 650 },
-    { nombre: "Limp Bizkit", precio: 600 },
-    { nombre: "Bad Omens", precio: 800 },
-];
-
-function agregarConciertosAlSelect(elemento, arrayDeConciertos) {
-    let html = '';
-    arrayDeConciertos.forEach(concierto => {
-        html += `<option value="${concierto.toLowerCase()}">${concierto}</option>`;
-    });
-    elemento.innerHTML = html;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const conciertoInput = document.getElementById('concierto');
     const cantidadInput = document.getElementById('cantidad');   
     const comprarBtn = document.getElementById('comprarBtn');    
     const resultadoDiv = document.getElementById('resultado');
     const resultadoDivCompraPrevia = document.getElementById('compraPrevia');
-  
+
+    const database =  [
+    {"nombre": "Slipknot", "precio": 500 },
+    { "nombre": "Metallica", "precio": 600 },
+    { "nombre": "Nirvana", "precio": 550 },
+    { "nombre": "Slipknot", "precio": 500 },
+    { "nombre": "Metallica", "precio": 600 },
+    { "nombre": "Nirvana", "precio": 550 },
+    { "nombre": "AC/DC", "precio": 700 },
+    { "nombre": "Led Zeppelin", "precio": 650 },
+    { "nombre": "Pink Floyd", "precio": 680 },
+    { "nombre": "Queen", "precio": 750 },
+    { "nombre": "The Beatles", "precio": 800 },
+    { "nombre": "The Rolling Stones", "precio": 720 },
+    { "nombre": "Guns N' Roses", "precio": 670 },
+    { "nombre": "Linkin Park", "precio": 600 },
+    { "nombre": "Green Day", "precio": 590 },
+    { "nombre": "The Who", "precio": 710 },
+    { "nombre": "Aerosmith", "precio": 640 },
+    { "nombre": "Red Hot Chili Peppers", "precio": 630 },
+    { "nombre": "Foo Fighters", "precio": 620 },
+    { "nombre": "Rammstein", "precio": 680 },
+    { "nombre": "The Doors", "precio": 690 },
+    { "nombre": "KISS", "precio": 610 },
+    { "nombre": "Pearl Jam", "precio": 650 },
+    { "nombre": "The Smashing Pumpkins", "precio": 620 },
+    { "nombre": "Deep Purple", "precio": 670 },
+    { "nombre": "Black Sabbath", "precio": 750 },
+    { "nombre": "Iron Maiden", "precio": 700 },
+    { "nombre": "System of a Down", "precio": 580 },
+    { "nombre": "Soundgarden", "precio": 590 },
+    { "nombre": "Pantera", "precio": 640 },
+    { "nombre": "Megadeth", "precio": 680 },
+    { "nombre": "Judas Priest", "precio": 710 },
+    { "nombre": "Rage Against the Machine", "precio": 650 },
+    { "nombre": "Avenged Sevenfold", "precio": 630 },
+    { "nombre": "Muse", "precio": 620 },
+    { "nombre": "Radiohead", "precio": 690 },
+    { "nombre": "The Strokes", "precio": 600 },
+    { "nombre": "The Killers", "precio": 620 },
+    { "nombre": "Arctic Monkeys", "precio": 610 },
+    { "nombre": "Motörhead", "precio": 700 },
+    { "nombre": "Disturbed", "precio": 650 },
+    { "nombre": "Evanescence", "precio": 620 },
+    { "nombre": "Marilyn Manson", "precio": 630 },
+    { "nombre": "The Cure", "precio": 680 },
+    { "nombre": "Fall Out Boy","precio": 610 },
+    { "nombre": "Bring Me the Horizon","precio": 590 },
+    { "nombre": "Blink-182", "precio": 600 },
+    { "nombre": "Paramore", "precio": 580 },
+    { "nombre": "Thirty Seconds to Mars", "precio": 620 },
+    { "nombre": "Breaking Benjamin", "precio": 590 },
+    { "nombre": "My Chemical Romance", "precio": 650 },
+    { "nombre": "Limp Bizkit", "precio": 600 },
+    { "nombre": "Bad Omens","precio": 800 }
+    ];
+
     const arrayDeNombresDeConciertos = database.map(concierto => concierto.nombre);
     agregarConciertosAlSelect(conciertoInput, arrayDeNombresDeConciertos);
 
-    
     cargarDatosPrevios();
+
+    function agregarConciertosAlSelect(elemento, arrayDeConciertos) {
+        let html = '';
+        arrayDeConciertos.forEach(concierto => {
+            html += `<option value="${concierto.toLowerCase()}">${concierto}</option>`;
+        });
+        elemento.innerHTML = html;
+    }
 
     function calcularCostoTotal(precioEntrada, cantidad) {
         return precioEntrada * cantidad;
@@ -82,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function procesarCompraConcierto(nombreConcierto, cantidadEntradas, precioEntrada) {
         const costoTotal = calcularCostoTotal(precioEntrada, cantidadEntradas);
         const ticketsCompradosStringify = localStorage.getItem('ticketsComprados');
-        const ticketsComprados = ticketsCompradosStringify || '[]'
+        const ticketsComprados = ticketsCompradosStringify || '[]';
         const ticketsCompradosParse = JSON.parse(ticketsComprados);
 
         const arrayConNuevoTicket = [
@@ -92,11 +91,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 cantidadEntradas: cantidadEntradas,
                 costoTotal: costoTotal
             }
-        ]
+        ];
 
         localStorage.setItem('ticketsComprados', JSON.stringify(arrayConNuevoTicket));
 
-        
         resultadoDiv.innerHTML = `
             <h2>Resumen de tu compra:</h2>
             <p>Concierto: ${nombreConcierto}</p>
@@ -113,21 +111,28 @@ document.addEventListener('DOMContentLoaded', function() {
         const cantidadEntradas = parseInt(cantidadInput.value);    
 
         if (isNaN(cantidadEntradas) || cantidadEntradas <= 0) {
-            alert('Por favor, ingrese una cantidad válida.');
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Por favor, ingrese una cantidad válida.',
+            });
             return;
-        } 
+        }
 
         const conciertoSeleccionado = database.find(concierto => concierto.nombre.toLowerCase() === nombreConcierto);
 
         if (!conciertoSeleccionado) {
-            alert('Concierto no encontrado.');
+            Swal.fire({
+                icon: 'error',
+                title: 'Concierto no encontrado',
+                text: 'El concierto seleccionado no existe en la base de datos.',
+            });
             return;
         }
 
         procesarCompraConcierto(conciertoSeleccionado.nombre, cantidadEntradas, conciertoSeleccionado.precio);
     });
 
-   
     function cargarDatosPrevios() {
         const compraGuardada = localStorage.getItem('ticketsComprados');
 
@@ -143,11 +148,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     </div>
                     <hr/>
                 `;
-            })
+            });
 
             resultadoDivCompraPrevia.innerHTML = html;
         }
     }
 });
-
-
